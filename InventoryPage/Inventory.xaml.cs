930b51f77b81d8b6aff21de4cb9ce96962506393
Tze Erhn <te.koay99@gmail.com>
2023-03-15 18:15:39 +0800

    Loaded database into datatable, bind to datagrid, manually create datagrid columns.

--- InventoryPage/Inventory.xaml.cs
+++ InventoryPage/Inventory.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,34 @@ namespace InventorySystem.InventoryPage
     /// <summary>
     /// Interaction logic for Inventory.xaml
     /// </summary>
+    
+    
+    
     public partial class Inventory : Page
     {
+
+        public string connectionString = "SERVER=localhost; DATABASE=inventory; UID=semi; PASSWORD=semitech;";
+
         public Inventory()
         {
             InitializeComponent();
+            loadIntoDataGrid();
+        }
+
+        private void loadIntoDataGrid()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string commandText = "SELECT * FROM inputs ORDER BY PartNum, BatchID";
+                MySqlCommand loadInventory = new MySqlCommand(commandText, connection);
+                connection.Open();
+                DataTable dt = new DataTable();
+                dt.Load(loadInventory.ExecuteReader());
+                connection.Close();
+                inventoryGrid.DataContext = dt;
+              }
         }
     }
+
+    
 }
