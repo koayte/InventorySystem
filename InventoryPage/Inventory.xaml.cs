f527e0fd39ae4e9026d5f64732a5df4ebe2c594e
Tze Erhn <te.koay99@gmail.com>
2023-03-17 10:23:19 +0800

    Changed style of search boxes.

--- InventoryPage/Inventory.xaml.cs
+++ InventoryPage/Inventory.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -102,7 +103,10 @@ namespace InventorySystem.InventoryPage
         {
             ApplyFilter();
         }
+
     }
 
-    
+
+
+
 }
---
+++ InventoryPage/WidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InventorySystem.InventoryPage
+{
+    public class WidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)(double)value - 5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
