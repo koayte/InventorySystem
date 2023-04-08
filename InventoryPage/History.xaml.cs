674f0ede0eea17f049baf23049d0f213535f88c2
Tze Erhn <te.koay99@gmail.com>
2023-04-08 16:20:52 +0800

    Added export function for History table.

--- InventoryPage/History.xaml.cs
+++ InventoryPage/History.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace InventorySystem.InventoryPage
 {
@@ -20,9 +22,43 @@ namespace InventorySystem.InventoryPage
     /// </summary>
     public partial class History : Page
     {
+        public string connectionString = "SERVER=localhost; DATABASE=inventory; UID=semi; PASSWORD=semitech;";
         public History()
         {
             InitializeComponent();
         }
+
+        // ------------------------------------------------------------ Export to .txt
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\htable.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            Export();
+        }
+
+        private void Export()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string commandText = "SELECT * FROM htable INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/htable.txt' " +
+                    "FIELDS TERMINATED BY ',' " +
+                    "ENCLOSED BY '\"' " +
+                    "LINES TERMINATED BY '\r\n'";
+                connection.Open();
+                using (MySqlCommand cmd = new MySqlCommand(commandText, connection))
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        MessageBox.Show("Export successful!");
+                    }
+                }
+                connection.Close();
+            }
+        }
     }
+
+    
 }
